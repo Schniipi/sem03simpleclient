ad4a5df77b16718c7a31af6a4918ced4402217b6
Schniipi <oliveraaron03@gmail.com>
2023-05-03 20:24:19 +0000

    Endret client

--- tcp_client.go
+++ tcp_client.go
@@ -12,12 +12,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-        
+	log.Println("os.Args[1] = ", os.Args[1])
+
         kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
         log.Println("Kryptert melding: ", string(kryptertMelding))
         _, err = conn.Write([]byte(string(kryptertMelding)))
-    
-	log.Println("os.Args[1] = ", os.Args[1])
 
  	_, err = conn.Write([]byte(os.Args[1]))
 	if err != nil {
